648e956776871d8c3a52143bb5aa62c7399b58f3
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-04-29 13:50:00 +0300

    Tallennus

--- Tehtävä-0.js
+++ Tehtävä-0.js
@@ -1,26 +1,27 @@
-var xhttp = new XMLHttpRequest()
+var browser = new XMLHttpRequest()
 
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    const data = JSON.parse(this.responseText)
-    console.log(data)
+browser.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200 && this.responseText) {
+        console.log(this.responseText);
+        console.log(this.status);
+        
+        const data = JSON.parse(this.responseText);
+        console.log(data)
 
-    var ul = document.createElement('ul')
-    ul.setAttribute('class', 'notes')
+        var ul = document.createElement("ul");
+        ul.setAttribute("class", "notes")
 
-    data.forEach(function(note){
-      var li = document.createElement('li')
-      
-      ul.appendChild(li);
-      li.appendChild(document.createTextNode(note.content))
-    })
+        data.forEach(function(note) {
+            var li = document.createElement("li");
 
-    document.getElementById("notes").appendChild(ul)
-  }
+            ul.appendChild(li);
+            li.appendChild(document.createTextNode(note.content));
+        })
+
+        document.getElementById("notes").appendChild(ul);
+    }
 }
 
-xhttp.open("GET", "Tehtävä-0.html", true)
-xhttp.send()
-
-
+browser.open("GET", "Tehtävä-0.json", true);
+browser.send()
 
