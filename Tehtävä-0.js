1cab88be0af42cb449ba96ac89cfab971dea20e7
Deadlyweather <tonyruotsalainenuusi@gmail.com>
2026-05-03 08:08:48 +0300

    Yhdistys

--- Tehtävä-0.js
+++ Tehtävä-0.js
@@ -1,26 +1,24 @@
-var xhttp = new XMLHttpRequest()
+// Testi 1
 
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    const data = JSON.parse(this.responseText)
-    console.log(data)
-
-    var ul = document.createElement('ul')
-    ul.setAttribute('class', 'notes')
-
-    data.forEach(function(note){
-      var li = document.createElement('li')
-      
-      ul.appendChild(li);
-      li.appendChild(document.createTextNode(note.content))
-    })
-
-    document.getElementById("notes").appendChild(ul)
-  }
+const getFrontPageHtml = noteCount => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Full stack -esimerkkisovellus</h1>
+          <p>muistiinpanoja luotu ${noteCount} kappaletta</p>
+          <a href="/notes">muistiinpanot</a>
+          <img src="kuva.png" width="200" />
+        </div>
+      </body>
+    </html>
+  `
 }
 
-xhttp.open("GET", "Tehtävä-0.html", true)
-xhttp.send()
-
-
-
+//app.get('/', (req, res) => {
+//  const page = getFrontPageHtml(notes.length)
+//  res.send(page)
+//})
