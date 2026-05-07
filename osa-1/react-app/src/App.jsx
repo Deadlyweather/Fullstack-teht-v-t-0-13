f5ba388ad7b40ca9fbb98e5aa697b9a5e334bffd
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-05-07 13:54:03 +0300

    Tehtävän annon tutkintaa

--- osa-1/react-app/src/App.jsx
+++ osa-1/react-app/src/App.jsx
@@ -1,10 +1,39 @@
+const Content = () => {
+  return (
+    <div>
+      <Part />
+      <Part />
+      <Part />
+    </div>
+  )
+}
 
-
-function App() {
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>Osa 1</h1>
+      <div>
+        <Header course={course} />
+        <Content parts={parts} />
+        <Total parts={parts} />
+      </div>
     </div>
   )
 }
