15fb2f64af4dde94938edc0b01f677a1199fb118
Deadlyweather <tonyruotsalainenuusi@gmail.com>
2026-05-15 14:26:43 +0300

    Testi lähetys

--- osa-1/anekdootit/src/App.jsx
+++ osa-1/anekdootit/src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
 
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const votes = { 0: 1, 1: 3, 2: 4, 3: 2 }
 
-function App() {
+  const copy = [ ...votes ]
   
+  copy[2] += 1     
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
+      <button onClick={() => setSelected((selected + 1) % anecdotes.length)}>Next anecdote</button>
+      <button onClick={() => copy[selected] += 1}>Vote</button>
+      {anecdotes[selected]}
 
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[2]}
+      <p>has {votes[2]} votes</p>
     </div>
   )
 }
