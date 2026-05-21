fb875361d1370963982581b21497eecf4106702b
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-05-21 08:21:32 +0300

    Osa 1 valmistus

--- osa-1/anekdootit/src/App.jsx
+++ osa-1/anekdootit/src/App.jsx
@@ -12,22 +12,53 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
-  const votes = { 0: 1, 1: 3, 2: 4, 3: 2 }
+  const [votes, setVotes] = useState([1, 4, 6, 3])
 
-  const copy = [ ...votes ]
-  
-  copy[2] += 1     
+  const [selected, setSelected] = useState(0)
+
+  const handleVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  const mostVotes = () => {
+    let maxVotes = 0
+    let maxIndex = 0
+    votes.forEach((vote, index) => {
+      if (vote > maxVotes) {
+        maxVotes = vote
+        maxIndex = index
+      }
+    })
+    return { votes: maxVotes, index: maxIndex }
+  }
+
+  const Anti_NAN = () => {
+    if (votes.length < anecdotes.length) {
+      const newVotes = [...votes]
+      while (newVotes.length < anecdotes.length) {
+        newVotes.push(0)
+      }
+      setVotes(newVotes)
+    }
+  }
 
   return (
     <div>
       <h1>Anecdote of the day</h1>
-      <button onClick={() => setSelected((selected + 1) % anecdotes.length)}>Next anecdote</button>
-      <button onClick={() => copy[selected] += 1}>Vote</button>
+      
       {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p><br />
+      <button onClick={() => { Anti_NAN(); setSelected((selected + 1) % anecdotes.length); }}>Next anecdote</button>
+      {console.log(`Index: ${selected}, Votes: ${votes[selected]}`)}
+      
+      
+      <button onClick={handleVote}>Vote</button><br />
 
       <h1>Anecdote with most votes</h1>
-      {anecdotes[2]}
-      <p>has {votes[2]} votes</p>
+      {anecdotes[mostVotes().index]}
+      <p>has {mostVotes().votes} votes</p>
     </div>
   )
 }
