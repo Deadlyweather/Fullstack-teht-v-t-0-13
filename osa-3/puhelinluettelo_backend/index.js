603021190c33db86e50bfe36a3ec635b36380220
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-08-25 11:55:05 +0300

    osa-3 päivitys

---
+++ osa-3/puhelinluettelo_backend/index.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+const baseUrl = 'http://localhost:3001/api/persons'
+
+
+app.use(cors())
+
+app.get(baseUrl, (request, response) => {
+  response.json({data1: 'Ello'})
+})
+
+// const getAll = () => {
+//   const request = axios.get(baseUrl)
+//   return request.then(response => response.data)
+// }
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+})
+
+// export default { getAll, create, update }
