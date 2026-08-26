f83f7b0147da8e6f79a660549fe7cf1a6f2fc509
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-08-26 10:10:14 +0300

    päivitys

--- osa-2/puhelinluettelo/src/Persons.js
+++ osa-2/puhelinluettelo/src/Persons.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-// const baseUrl = 'http://localhost:3001/persons'
-const baseUrl = 'http://localhost:10000/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   console.log('getAll')
