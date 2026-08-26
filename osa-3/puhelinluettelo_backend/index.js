ad9a411f3a1baa6c90384cba3133a1e34e71a1bf
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-08-26 09:52:10 +0300

    pika päivitys

--- osa-3/puhelinluettelo_backend/index.js
+++ osa-3/puhelinluettelo_backend/index.js
@@ -16,7 +16,7 @@ app.get("/persons", (request, response) => {
   response.json(persons)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 10000
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
 })
