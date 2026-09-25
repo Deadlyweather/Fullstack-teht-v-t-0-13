fefb36088720fde3a5d63b836d5daab74f72a409
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-09-25 08:42:57 +0300

    päivitys

--- osa-3/puhelinluettelo_mongo/index.js
+++ osa-3/puhelinluettelo_mongo/index.js
@@ -1,6 +1,54 @@
+require('dotenv').config()
+const express = require('express')
 const mongoose = require('mongoose')
+const Person = require('./mongo')
+
+const app = express()
+app.use(express.json())
 
 mongoose.set('strictQuery', false)
+
+app.get('/api/persons', (request, response) => {
+  Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => response.status(500).json({ error: error.message }))
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = new Person(request.body)
+  person.save()
+    .then(savedPerson => response.status(201).json(savedPerson))
+    .catch(error => response.status(400).json({ error: error.message }))
+})
+
+app.put('/api/persons/:id', (request, response) => {
+  Person.findById(
+    request.params.id,
+    request.body,
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => response.status(400).json({ error: error.message }))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+    .then(deletedPerson => {
+      if (deletedPerson) {
+        response.status(204).end()
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => response.status(400).json({ error: error.message }))
+})
+
 
 const url = process.env.MONGODB_URI
 
@@ -13,19 +61,8 @@ mongoose.connect(url, { family: 4 })
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
 
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-module.exports = mongoose.model('Person', personSchema)
-
-const Person = require("./models/person")
--- osa-3/puhelinluettelo_mongo/mongo.js
+++ osa-3/puhelinluettelo_mongo/mongo.js
@@ -1,21 +1,29 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-if (process.argv.length < 3) {
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.models.Person || mongoose.model('Person', personSchema)
+
+module.exports = Person
+
+if (require.main === module && process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
-})
-
-module.exports = mongoose.model('Person', personSchema)
-
-const Person = mongoose.model('Person', personSchema)
-
-if (process.argv[3] != undefined && process.argv[4] != undefined) {
+if (require.main === module && process.argv[3] != undefined && process.argv[4] != undefined) {
 
   const person = new Person({
     name: process.argv[3],
@@ -26,7 +34,7 @@ if (process.argv[3] != undefined && process.argv[4] != undefined) {
     console.log(`added ${person.name} number ${person.number} to phonebook`)
     mongoose.connection.close()
   })
-} else {
+} else if (require.main === module) {
   const phonebook = []
   console.log('phonebook:')
   Person.find({}).then(result => {
